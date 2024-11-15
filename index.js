2f785e0c0c7e308d5f41c02921b4417ec811a824
Fi <158217226+lightfi7@users.noreply.github.com>
2024-11-14 20:00:44 -0800

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const puppeteer = require('puppeteer');
 // const browser = await puppeteer.launch();
 // const page = await browser.newPage();
 
+let x_xsrf_token = '';
 
 const login = async (page) => {
     await page.goto('https://affilisting.com/login');
@@ -13,22 +14,36 @@ const login = async (page) => {
     await page.type('#password', process.env.PASSWORD);
     await page.click("button[type='submit']");
     await page.waitForNetworkIdle();
-
 }
 
 const fetch = async (page) => {
     const data = await page.evaluate(async () => {
-        const response = await fetch('https://affilisting.com/list', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
+        return new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest();
+            xhr.open('GET', 'https://affilisting.com/list', true);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.setRequestHeader("x-xsrf-token", x_xsrf_token);
+            xhr.withCredentials = true;
+
+            xhr.onload = function () {
+                if (xhr.status >= 200 && xhr.status < 300) {
+                    resolve(JSON.parse(xhr.responseText));
+                } else {
+                    reject(new Error('Request failed with status ' + xhr.status));
+                }
+            };
+
+            xhr.onerror = function () {
+                reject(new Error('Network error'));
+            };
+
+            xhr.send();
         });
-        return response.json();
     });
 
     console.log(data);
 }
+
 
 (async () => {
     const browser = await puppeteer.launch({
@@ -45,6 +60,16 @@ const fetch = async (page) => {
         deviceScaleFactor: 1,
         isMobile: false
     });
+
+    await page.setRequestInterception(true);
+
+    page.on('request', (request) => {
+        if (request.url().startsWith('https://affilisting.com/')) {
+            x_xsrf_token = request.headers()['x-xsrf-token'];
+        }
+        page.setRequestInterception(false);
+    })
+
 
     await login(page);
     await fetch(page);
