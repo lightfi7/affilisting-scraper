1be4a7c0309394797d206420192570043476c548
Fi <158217226+lightfi7@users.noreply.github.com>
2024-11-24 08:57:37 -0800

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,48 @@ const mongoose = require('mongoose');
 const Program = require('./models/program');
 const cheerio = require('cheerio');
 const axios = require('axios');
+const https = require("https");
+
+let currentPage = 1;
+let xsrfToken = '', cookies, cookieString;
+axios.maxRedirects = 0;
+const agent = new https.Agent({
+    rejectUnauthorized: false,
+});
+
 
 
 mongoose.connect(process.env.MONGODB_URI, {})
     .then(() => console.log('Connected to MongoDB'))
     .catch(error => console.error('Failed to connect to MongoDB:', error));
 
-let n = 1;
-let x_xsrf_token = '';
+async function makeRequest(url) {
+    return await axios.get(url, {
+        headers: {
+            Cookie: cookieString,
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+        },
+        httpsAgent: agent,
+    });
+}
 
-const doLogin = async (page) => {
+function handleAxiosError(error) {
+    let errorMsg;
+    console.log("Error Code:", error.code);
+
+    if (["ERR_BAD_RESPONSE", "ERR_BAD_REQUEST"].includes(error.code)) {
+        errorMsg = error.request.res.responseUrl;
+    } else if (["ETIMEDOUT", "ENOTFOUND", "ECONNRESET", "ERR_FR_TOO_MANY_REDIRECTS", "UNABLE_TO_VERIFY_LEAF_SIGNATURE", "ERR_TLS_CERT_ALTNAME_INVALID"].includes(error.code)) {
+        errorMsg = error.request._options.href;
+    } else {
+        console.log(error);
+        errorMsg = "";
+    }
+
+    return errorMsg;
+}
+
+const loginToWebsite = async (page) => {
     try {
         await page.goto('https://affilisting.com/login', { waitUntil: 'networkidle2', timeout: 300000 });
         await page.waitForSelector('#email', { timeout: 300000 });
@@ -23,224 +55,184 @@ const doLogin = async (page) => {
         await page.click("button[type='submit']");
         await page.waitForNetworkIdle();
 
+        cookies = await page.cookies();
+        cookieString = cookies.map(cookie => `${cookie.name}=${cookie.value}`).join("; ");
     } catch (error) {
         console.error('Login failed:', error);
     }
 };
 
-const getRefs = async (page, uuid) => {
+const retrieveReferences = async (uuid) => {
     try {
-        console.log(uuid)
-        const response = await page.goto(`https://affilisting.com/redirect/${uuid}/apply`, {
-            waitUntil: 'networkidle2'
-        });
-        let link = response.url();
-        const url = new URL(link);
-        link = 'https://' + url.hostname;
-        await page.goto(link, { waitUntil: 'networkidle2', timeout: 300000 });
-        const html = await page.content();
-        const $ = cheerio.load(html);
-        const socials = getSocials(html);
-        const description = $('meta[name="description"]').attr('content');
-        const image = $('meta[property="og:image"]').attr('content');
-        return {
-            link,
-            description,
-            image,
-            socials,
-        };
+        let response = await makeRequest(`https://affilisting.com/redirect/${uuid}/apply`, cookieString);
+
+        const redirectedUrl = response.request.res.responseUrl;
+        console.log('Redirected to:', redirectedUrl);
+
+        const urlObject = new URL(redirectedUrl);
+        const parsedRedirectedUrl = 'https://' + urlObject.hostname;
+
+        response = await makeRequest(parsedRedirectedUrl, cookieString);
+        const htmlContent = response.data;
+        const $ = cheerio.load(htmlContent);
+
+        const socialMediaLinks = extractSocialLinks(htmlContent);
+        const metaDescription = $('meta[name="description"]').attr('content');
+        const metaImage = $('meta[property="og:image"]').attr('content');
+
+        return { link: redirectedUrl, description: metaDescription, image: metaImage, socials: socialMediaLinks };
     } catch (error) {
         console.error('Failed to fetch redirected link:', error);
-        return {
-            link: null,
-            description: null,
-            image: null,
-            socials: [],
-        };
+        return { link: null, description: null, image: null, socials: [] };
     }
 };
 
-
-const getSocials = (html) => {
+const extractSocialLinks = (htmlContent) => {
     try {
-        const $ = cheerio.load(html, { decodeEntities: false });
-        const pattern =
-            /(?:https?:\/\/)?(?:www\.)?(?:facebook|fb|twitter|linkedin|instagram|youtube)\.com\/(?:[\w\-\.]+\/?)+/g;
-        const links = new Set();
+        const $ = cheerio.load(htmlContent, { decodeEntities: false });
+        const socialMediaPattern = /(?:https?:\/\/)?(?:www\.)?(?:facebook|fb|twitter|linkedin|instagram|youtube)\.com\/(?:[\w\-\.]+\/?)+/g;
+
+        const linksSet = new Set();
+
         $("a").each((_, anchor) => {
-            const href = $(anchor).attr("href");
-            if (href && pattern.test(href)) {
-                links.add(href);
+            const hrefValue = $(anchor).attr("href");
+            if (hrefValue && socialMediaPattern.test(hrefValue)) {
+                linksSet.add(hrefValue);
             }
         });
-        return [...links];
+
+        return [...linksSet];
     } catch (error) {
         console.error("Error extracting data from HTML:", error);
         return [];
     }
 };
 
-
-const getPrograms = async (page) => {
+const fetchProgramsData = async (page) => {
     try {
-        const json_string = await page.evaluate(({ x_xsrf_token, page }) => {
+        const jsonResponseString = await page.evaluate(({ xsrfToken, currentPage }) => {
             return new Promise((resolve, reject) => {
-                const xhr = new XMLHttpRequest();
-                xhr.open('GET', `https://affilisting.com/list?page=${page}`, true);
-                xhr.setRequestHeader('Content-Type', 'application/json');
-                xhr.setRequestHeader("x-inertia", true);
-                xhr.setRequestHeader("x-inertia-version", 'fc9556c08ecbf859722f010bee10ca59');
-                xhr.setRequestHeader("x-requested-with", 'XMLHttpRequest');
-                xhr.setRequestHeader("x-xsrf-token", x_xsrf_token);
-                xhr.withCredentials = true;
-                xhr.onload = function () {
-                    if (xhr.status >= 200 && xhr.status < 300) {
-                        resolve(xhr.responseText);
+                const xhrRequest = new XMLHttpRequest();
+                xhrRequest.open('GET', `https://affilisting.com/list?page=${currentPage}`, true);
+                xhrRequest.setRequestHeader('Content-Type', 'application/json');
+                xhrRequest.setRequestHeader("x-inertia", true);
+                xhrRequest.setRequestHeader("x-inertia-version", 'fc9556c08ecbf859722f010bee10ca59');
+                xhrRequest.setRequestHeader("x-requested-with", 'XMLHttpRequest');
+                xhrRequest.setRequestHeader("x-xsrf-token", xsrfToken);
+                xhrRequest.withCredentials = true;
+
+                xhrRequest.onload = function () {
+                    if (xhrRequest.status >= 200 && xhrRequest.status < 300) {
+                        resolve(xhrRequest.responseText);
                     } else {
-                        reject(new Error('Request failed with status ' + xhr.status));
+                        reject(new Error('Request failed with status ' + xhrRequest.status));
                     }
                 };
-                xhr.onerror = function () {
-                    reject(new Error('Network error'));
-                };
-                xhr.send();
+
+                xhrRequest.onerror = function () { reject(new Error('Network error')); };
+                xhrRequest.send();
             });
-        }, { x_xsrf_token, page: n });
+        }, { xsrfToken, currentPage });
 
-        const json_data = JSON.parse(json_string);
-        let { data = [], next_page_url = undefined } = json_data.props.affiliates;
+        const jsonDataParsed = JSON.parse(jsonResponseString);
 
-        console.log(data)
+        let { data: affiliatesData = [], next_page_url } = jsonDataParsed.props.affiliates;
 
         if (next_page_url) {
-            n = n + 1;
-        } else
-            n = 1;
+            currentPage++;
+        } else {
+            currentPage = 1;
+        }
 
-        data = data.map(item => {
-            const tags = item.tags.map(tag => tag.id);
-            const commission_type = item.type?.machine_name;
-            const langs = item.langs.map(item => item.id)
-            const platform = item.platform?.id;
-
-            return { ...item, tags, langs, platform, commission_type };
-        })
-
-        for (let i = 0; i < data.length; i++) {
-
-            const {
-                link,
-                description,
-                image,
-                socials
-            } = await getRefs(page, data[i].uuid);
-
-            console.log(link);
-            console.log(description);
-            console.log(image);
-            console.log(socials);
-
-            data[i].link = link;
-            data[i].description = description;
-            data[i].image = image;
-            data[i].socials = socials;
+        for (let i = 0; i < affiliatesData.length; i++) {
+            let item = affiliatesData[i];
+            const tagsArray = item.tags.map(tag => tag.id);
+            const commissionTypeValue = item.type?.machine_name;
+            const languagesArray = item.langs.map(langItem => langItem.id);
+            const platformValue = item.platform?.id;
+            Object.assign(item, { tags: tagsArray, langs: languagesArray, platform: platformValue, commission_type: commissionTypeValue });
+            const { link, description, image, socials = [] } = await retrieveReferences(item.uuid);
+            item.link = link;
+            item.description = description;
+            item.image = image;
+            item.socials = socials;
             await new Promise((resolve, reject) => setTimeout(resolve, 3000));
         }
 
-        await Program.insertMany(data);
+        Program.insertMany(affiliatesData).catch(err => console.error('Database insertion failed:', err));
 
     } catch (error) {
         console.error('Data fetch failed:', error);
     }
 };
 
-
 (async () => {
-    console.log(`Scraping page ${n}`);
+    console.log(`Scraping page ${currentPage}`);
+
     try {
         const browser = await puppeteer.launch({
-            headless: true,
+            headless: false,
             args: ['--no-sandbox', '--disable-setuid-sandbox'],
-            env: {
-                DISPLAY: ":10.0"
-            }
+            // env: { DISPLAY: ":10.0" } 
         });
 
         const page = await browser.newPage();
-        await page.setViewport({
-            width: 1280,
-            height: 768,
-            deviceScaleFactor: 1,
-            isMobile: false
-        });
 
-        // Set request interception to capture the XSRF token
+        await page.setViewport({ width: 1280, height: 768, deviceScaleFactor: 1, isMobile: false });
+
         await page.setRequestInterception(true);
+
         page.on('request', (request) => {
             if (request.url().startsWith('https://affilisting.com/')) {
-                x_xsrf_token = request.headers()['x-xsrf-token'];
+                xsrfToken = request.headers()['x-xsrf-token'];
             }
             request.continue();
         });
 
-        // Validate environment variables
         if (!process.env.EMAIL || !process.env.PASSWORD) {
             throw new Error('EMAIL and PASSWORD must be set in environment variables.');
         }
 
-        await doLogin(page);
-        await getPrograms(page);
+        await loginToWebsite(page);
+        await fetchProgramsData(page);
 
         await page.screenshot({ path: 'example.png' });
-
-        await browser.close(); // Ensure the browser closes regardless of errors
-
+        await browser.close();
     } catch (error) {
         console.error('An error occurred:', error);
-    } finally {
     }
 })();
 
 cron.schedule('*/60 * * * *', async () => {
-    console.log(`Scraping page ${n}`);
+    console.log(`Scraping page ${currentPage}`);
+
     try {
-        const browser = await puppeteer.launch({
-            headless: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox'],
-            env: {
-                DISPLAY: ":10.0"
-            }
-        });
+        const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'], env: { DISPLAY: ":10.0" } });
 
         const page = await browser.newPage();
-        await page.setViewport({
-            width: 1280,
-            height: 768,
-            deviceScaleFactor: 1,
-            isMobile: false
-        });
 
-        // Set request interception to capture the XSRF token
+        await page.setViewport({ width: 1280, height: 768, deviceScaleFactor: 1, isMobile: false });
+
         await page.setRequestInterception(true);
+
         page.on('request', (request) => {
             if (request.url().startsWith('https://affilisting.com/')) {
-                x_xsrf_token = request.headers()['x-xsrf-token'];
+                xsrfToken = request.headers()['x-xsrf-token'];
             }
             request.continue();
         });
 
-        // Validate environment variables
         if (!process.env.EMAIL || !process.env.PASSWORD) {
             throw new Error('EMAIL and PASSWORD must be set in environment variables.');
         }
 
-        await doLogin(page);
-        await getPrograms(page);
+        await loginToWebsite(page);
+        await fetchProgramsData(page);
 
         await page.screenshot({ path: 'example.png' });
     } catch (error) {
         console.error('An error occurred:', error);
     } finally {
-        await browser.close(); // Ensure the browser closes regardless of errors
+
     }
-})
+});
